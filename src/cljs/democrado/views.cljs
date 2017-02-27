bfa5622503d2c68434f0989b343544c597d17fd6
Anonymous Applicant <anonymous@example.com>
2017-02-27 15:35:58 -0600

    Adding about page text

--- src/cljs/democrado/views.cljs
+++ src/cljs/democrado/views.cljs
@@ -50,7 +50,25 @@
   [:div.row
    [:div.col-md-12
     [:h2 "About Democrado"]
-    "Democrado is a take home assignment for my Democracy Works job application. In a future commit I will describe it in more detail here."]])
+    [:p "
+Democrado is a small to-do list application implemented as part of my Democracy
+Works job application. I admittedly went slightly overboard with it, but it
+seemed like a good opportunity to experiment with some libraries I'd been
+interested in for a while (Yada, bidi, and re-frame). If you're interested in
+what I can do within a limited time, you can stop at
+52ed2db5c960710289b94a48b81b9e428934caa2, that version implements all the basic
+functionality, but is lacking some refinements."
+     ]
+    [:p "
+To summarize how the application works, it uses Yada to serve RESTful HTTP
+resources, bidi to route to those resources, and re-frame to interact with them
+and render the client side user interface. Datomic is used as the backing data
+store. Datomic is probably overkill for an application this simple, but the
+assignment suggested the application should be built with extension in mind.
+Given that requirement, I think it's a reasonable choice. Boot was chosen as a
+build tool because the composability of its tasks (very useful for asset
+pipelines)."
+     ]]])
 
 (defn main-panel []
   [:div.container
